d5db93e80869c737ed845f933f8c18b138359d31
Poisoned-Honey <poisonedhoney13@gmail.com>
2021-09-29 22:29:54 -0700

    make poop turret work(hopefully)

--- src/example/content/ExampleBlocks.java
+++ src/example/content/ExampleBlocks.java
@@ -1,7 +1,13 @@
 package example.content;
 
+import mindustry.content.Bullets;
+import mindustry.content.Fx;
+import mindustry.content.Items;
 import mindustry.ctype.ContentList;
+import mindustry.gen.Sounds;
+import mindustry.type.Category;
 import mindustry.world.Block;
+import mindustry.world.blocks.defense.turrets.ItemTurret;
 import mindustry.world.blocks.defense.turrets.Turret;
 
 public class ExampleBlocks implements ContentList {
@@ -9,9 +15,27 @@ public class ExampleBlocks implements ContentList {
 
     @Override
     public void load() {
-        turretPoop = new Turret("poop-turret"){{
-            health = 1000;
-            shots = 3;
+        turretPoop = new ItemTurret("poopturret"){{
+            requirements(Category.turret, with(Items.copper, 900, Items.graphite, 300, Items.surgeAlloy, 250, Items.plastanium, 175, Items.thorium, 250));
+            ammo(
+                    Items.graphite, Bullets.standardDenseBig,
+                    Items.pyratite, Bullets.standardIncendiaryBig,
+                    Items.thorium, Bullets.standardThoriumBig
+            );
+            reloadTime = 7f;
+            range = 260f;
+            inaccuracy = 3f;
+            recoilAmount = 3f;
+            spread = 8f;
+            shootShake = 2f;
+            shots = 2;
+            size = 4;
+            shootCone = 24f;
+            shootSound = Sounds.shootBig;
+
+            health = 160 * size * size;
+
+            limitRange();
         }};
     }
 }
